ad1477ea9f688bc1f8bf48ec718abf8609bc23b5
clayton <clayton@laptop>
2019-10-13 10:51:51 -0400

    fixed string unicode support

--- src/lexer/lexerString.c
+++ src/lexer/lexerString.c
@@ -2,6 +2,7 @@
 #include "../../include/e4c/e4c.h"
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 E4C_DEFINE_EXCEPTION(MalformedString,"Malformed String",RuntimeException);
 E4C_DEFINE_EXCEPTION(TooManyCharactors,"A charactor type only allows 1 charactor",RuntimeException);
 const int incrementSize=64;
@@ -12,14 +13,13 @@ U8* __lexerLexString(U8* input,U64* _length,U8 endChar,U64 maximumRuns) {
 	U8* where=text;
 	U64 reservedLength=incrementSize;
 	U64 actualLength=0;
+	bool forceOne=false;
 	while(*input!=endChar) {
 		char* invalidP=invalidStringTerminators;
 		while(*invalidP!=0)
 			if(*invalidP==0||*(invalidP++)==*where)
 				throw(MalformedString,where);
-		if(*(_length--)==0)
-			throw(MalformedString,NULL);
-		const char* sequencesPairs[][2]={
+		const char* sequencesPairs[12][2]={
 										{"\\\\","\\"},
 										{"\\a","\a"},
 										{"\\b","\b"},
@@ -31,12 +31,11 @@ U8* __lexerLexString(U8* input,U64* _length,U8 endChar,U64 maximumRuns) {
 										{"\\v","\v"},
 										{"\\'","'"},
 										{"\\\"","\""},
-										{"\\?","\?"}
-							 
+										{"\\?","\?"} //12
 		};
-		const size_t count=sizeof(sequencesPairs)/sizeof(char*);
+		const size_t count=12;
 		void addText(char* literal) {
-			int length=strlen(literal);
+			int length=!forceOne?strlen(literal):1;
 			if(reservedLength<length+actualLength) {
 				U64 offset=where-text;
 				text=realloc(text,actualLength+reservedLength);
@@ -44,48 +43,32 @@ U8* __lexerLexString(U8* input,U64* _length,U8 endChar,U64 maximumRuns) {
 			}
 			memcpy(where,literal,length);
 			where+=length;
+			forceOne=false;
 		}
 		for(int i=0;i!=count;i++) {
 			U8* sequence=sequencesPairs[i][0];
 			if(0==strncmp(input,sequence,strlen(sequence))) {
 				U8* literal=sequencesPairs[i][1];
+				U8* old=input;
 				addText(literal);
+				input=old+strlen(sequencesPairs[i][0]);
 				break;
 			}
 		}
 		U64 getHexDigitsAndParse(U8 count) {
-			U64 getDigitValue(U8 charactor) {
-				if('0'<=charactor&&charactor<='9')
-					return charactor-'0';
-				if('a'<=charactor&&charactor<='f')
-					return charactor-'a'+10;
-				if('A'<=charactor&&charactor<='F')
-					return charactor-'A'+10;
-				return -1;
-			}
-			U64 multiplyBy=1;
 			U64 value=0;
-			for(U8 i=0;i!=count;i++) {
-				U64 digit=getDigitValue(input[count]);
-				if(digit==-1) {
-					const U8* msg="%c: is not a hexidecimal digit\n\x00";
-					const U64 msgSize=sizeof(msg);
-					char text[msgSize+1];
-					vsprintf(text,msg,input[count]);
-					throw(LexicalError,text);
-				}
-				value+=multiplyBy*digit;
-				multiplyBy*=16;
-			}
+			sscanf(input+2,"%lx",&value);
+			return value;
 		}
 		//hex
-		if(strncmp(input,"\\x",2)) {
+		if(0==strncmp(input,"\\x",2)) {
 			getHexDigitsAndParse(2);
 			U8* toWrite[3];
 			strncpy(toWrite,input,2);
-			toWrite[2]=0;
+			toWrite[1]=0;
+			forceOne=true;
 			addText(toWrite);
-			where+=2;
+			input+=4;
 			continue;
 		}
 		//octal
@@ -112,7 +95,7 @@ U8* __lexerLexString(U8* input,U64* _length,U8 endChar,U64 maximumRuns) {
 				vsprintf(errMsg,message,where[i]);
 				throw(LexicalError,errMsg);
 			}
-			where+=i;
+			input+=i;
 			char toAdd[2];
 			toAdd[1]=NULL;
 			*toAdd=value;
@@ -122,35 +105,44 @@ U8* __lexerLexString(U8* input,U64* _length,U8 endChar,U64 maximumRuns) {
 		const U64 OneByteUTF8Size=(1<<7)-1;
 		const U64 TwoByetUTF8Size=(1<<(6+6))-1;
 		const U64 ThreeByteUTF8Size=(1<<(5+6+6))-1;
-		const U64 FoutByteUTF8Size=(1<<(4+6+6+6))-1;
+		const U64 FourByteUTF8Size=(1<<(4+6+6+6))-1; //TODO implement me
 	
-		if(0==strncmp(input,"\\u",2)|0==strncmp(input,"\\U",2)) {
+		if(0==strncmp(input,"\\u",2)||0==strncmp(input,"\\U",2)) {
 			U64 value;
+			U8 bytes=1;
+			U8 mask;
+			U8 buffer[5];
 			if(input[1]=='U')
-				input=getHexDigitsAndParse(8);
+				value=getHexDigitsAndParse(8);
 			else
-				input=getHexDigitsAndParse(4);
-
+				value=getHexDigitsAndParse(4);
 			if(value<=OneByteUTF8Size) {
-				*(input+1)=value&(0xff>>1);
+				U8 ch[2]={NULL,NULL};
+				*ch=value&(0xff>>1);
+				forceOne=true;
+				addText(ch);
+				input+=2+(input[1]=='U'?8:4);
 				continue;
+			} else {
+				mask=0xff<<(8-bytes);
+				if(value<=TwoByetUTF8Size)
+					bytes=2;
+				else if(value<=ThreeByteUTF8Size)
+					bytes=3;
+				else if(value<=FourByteUTF8Size)
+					bytes=4;	
+				
+				for(int i=bytes-1;i!=0;i--) {
+					buffer[i]=(value&0b00111111)|0b10000000;
+					value>>=6;
+				}
 			}
-			U8 bytes=0;
-			if(value<=TwoByetUTF8Size)
-				bytes=2;
-			if(value<=ThreeByteUTF8Size)
-				bytes=3;
-			U8 mask=0xff<<(8-bytes);
-			char buffer[bytes+1];
-			for(int i=bytes-1;i!=0;i--) {
-				buffer[i]=value&0b00111111;
-				value>>=6;
-			}
+			mask=0|(0xff<<(8-(bytes)));
 			buffer[0]=(value)|mask;
 			buffer[bytes]=NULL;
 			addText(buffer);
-			U64 inc=(input[1]=='U')?8:4;
-			where+=bytes;
+			U64 inc=2+(input[1]=='U'?8:4);
+			//where+=bytes;
 			input+=inc;
 			continue;
 		}
@@ -164,16 +156,16 @@ U8* __lexerLexString(U8* input,U64* _length,U8 endChar,U64 maximumRuns) {
 			text=realloc(text,reservedLength+incrementSize);
 			where=text+offset;
 		}
-		*(where++)=input[0];
-		input++;
+		*(where++)=*(input++);
 	}
 	*_length=where-text;
 	return text;
 }
 U8* lexerLexString(U8* text,U64* length) {
 	if(*text=='"') {
+		text++;
 		U8* retVal=__lexerLexString(text,length,'"',-1);
-		length[0]++;
+		length[0]+=2;
 		return retVal;
 	}
 	return NULL;
